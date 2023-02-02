0ea10beac9e9bb64be349a2ffd8b03ac5521fe89
Juno <junoslg885@gmail.com>
2023-02-02 11:06:52 +0530

    Updated meth 1 function of lamda expression

--- lamda_expression.java
+++ lamda_expression.java
@@ -1,16 +1,26 @@
 //Lambda expression (Can be cast/used only to functional interfaces)
 
+import java.util.Scanner;
+
 @FunctionalInterface
 interface test {
-    void meth1();
+    int meth1(int a, int b);
 }
 
 public class lamda_expression {
     public static void main(String[] args) {
-        test ob = () -> {
+        test ob = (int a, int b) -> {
             // overriding meth1() from test interface
-            System.out.println("Hello world!");
+            int s;
+            s = a + b;
+            return s;
         };
-        ob.meth1();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two values: ");
+        System.out.print("=> ");
+        int a = sc.nextInt();
+        System.out.print("=> ");
+        int b = sc.nextInt();
+        System.out.println(ob.meth1(a, b));
     }
 }
